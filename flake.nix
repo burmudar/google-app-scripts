85ab1ba42cd1233b535e9f334adf8aea8f224011
William Bezuidenhout <william.bezuidenhout@sourcegraph.com>
2023-03-05 07:51:41 +0200

    move to flakes

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Node Development environment";
+
+  inputs = {
+    nixpkgs = { url = "github:NixOS/nixpkgs/nixpkgs-unstable"; };
+    flake-utils = { url = "github:numtide/flake-utils"; };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... } : {
+    flake-utils.lib.eachDefaultSystem { system:
+      let
+        inherit system;
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs-18_x
+            nodePackages.typescript
+            nodePackages.typescript-language-server
+            nodePackages.clasp
+            esbuild
+            pnpm
+          ];
+        }
+      }
+  };
+}
